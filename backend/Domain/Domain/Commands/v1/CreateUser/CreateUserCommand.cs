b7a0a7e8c1cb89e19052ceea1e7c7ea7f217be89
Thaynnara Santos <thaynnaraluciano@gmail.com>
2024-12-05 16:16:36 -0300

    chore: add domain layer project

---
+++ backend/Domain/Domain/Commands/v1/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Commands.v1.CreateUser
+{
+    public class CreateUserCommand
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
